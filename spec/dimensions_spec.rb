921f84ed42cd766a0597f35d407a939a8d4113e2
Roland Burrows <rolandnburrows@gmail.com>
2017-04-17 21:14:02 -0600

    Add fastimage dimensionality analysis to spec

--- spec/dimensions_spec.rb
+++ spec/dimensions_spec.rb
@@ -4,8 +4,12 @@ describe 'Emoji dimensions' do
 
   it 'conform to length, width, and size requirements' do
     Dir["./custom_emoji/*"].each { |emoji_image|
-      # Files must be less than 64kb in size
+      puts "Verifying file (#{File.basename(emoji_image)})"
+      # Files must have size less than 64kb
       expect(File.new(emoji_image).size).to be < 64000
+      # Files must have width less than 128px
+      expect(FastImage.size(emoji_image)[0]).to be <= 128
+      expect(FastImage.size(emoji_image)[1]).to be <= 128
     }
   end
 
