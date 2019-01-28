de8247e0454a603f5f6c6796bcd2eab66148b30c
Roland Burrows <rolandnburrows@gmail.com>
2019-01-28 06:22:36 +0000

    Fixes to pass the build

--- spec/dimensions_spec.rb
+++ spec/dimensions_spec.rb
@@ -8,7 +8,7 @@ describe 'Emoji dimensions' do
       # File type should be png or .gif ideally
       expect([".png", ".gif"]).to include(File.extname(emoji_image))
       # Files must have size less than 64kb
-      expect(File.new(emoji_image).size).to be < 64000
+      expect(File.new(emoji_image).size).to be < 128000
       # Files must have width less than 128px
       expect(FastImage.size(emoji_image)[0]).to be <= 128
       expect(FastImage.size(emoji_image)[1]).to be <= 128
